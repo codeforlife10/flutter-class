de0d46c64e8b54cde0bbed7473092e9e9e14b974
codeforlife10 <lamaraman536536@gmail.com>
2022-05-30 12:14:21 +0545

    learned control statement

---
+++ Basic/if_else.dart
@@ -0,0 +1,12 @@
+main() {
+  String day = 'Wednesday';
+  if (1 < 5 || day == 'Wednesday') {
+    print('1 is less than 5');
+  } else {
+    print('could not satisfy contidion');
+  }
+
+  if (day == 'sunday') {
+    print('Today is sunday');
+  } else if (day == 'Monday') ;
+}
