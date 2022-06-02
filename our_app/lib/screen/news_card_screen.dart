3b585fc28f92125371184ea8f5bdb3b1bbc08a35
codeforlife10 <lamaraman536536@gmail.com>
2022-06-02 13:59:10 +0545

    expanded flexible ,media query

--- our_app/lib/screen/news_card_screen.dart
+++ our_app/lib/screen/news_card_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class NewsCard extends StatelessWidget {
@@ -7,6 +5,10 @@ class NewsCard extends StatelessWidget {
       "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg";
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    print("Screen width is : $width");
+    print("Screen width is : $height");
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -21,53 +23,59 @@ class NewsCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  // color: Colors.green,
-                  width: 250,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Mr Journalist",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Real Madrid are the European Champion",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    // color: Colors.green,
+                    // width: 250,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Mr Journalist",
+                          style: TextStyle(color: Colors.blue),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      // Row(
-                      //   children: [
-                      //     Text("Economics"),
-                      //     SizedBox(width: 20),
-                      //     Text("|"),
-                      //     SizedBox(width: 20),
-                      //     Text("31st May 2020"),
-                      //   ],
-                      // ),
-                      Text(
-                        "Ecomonics   |   31st May 2022",
-                        style: TextStyle(
-                          color: Colors.blue,
+                        SizedBox(height: 10),
+                        Text(
+                          "Real Madrid are the European Champion",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      )
-                    ],
+                        SizedBox(height: 10),
+                        // Row(
+                        //   children: [
+                        //     Text("Economics"),
+                        //     SizedBox(width: 20),
+                        //     Text("|"),
+                        //     SizedBox(width: 20),
+                        //     Text("31st May 2020"),
+                        //   ],
+                        // ),
+                        Text(
+                          "Ecomonics   |   31st May 2022",
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-                Container(
-                  // color: Colors.red,
-                  // width: 100,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      image,
-                      width: 100,
-                      height: 70,
-                      fit: BoxFit.cover,
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    // color: Colors.red,
+                    // width: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        image,
+                        width: 100,
+                        height: 70,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
--- our_app/lib/screen/row_and_column_screen.dart
+++ our_app/lib/screen/row_and_column_screen.dart
@@ -10,50 +10,58 @@ class RowAndColumnScreen extends StatelessWidget {
         // color: Colors.grey,
         // height: double.infinity,
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            NewsCard(),
-            Container(
-                height: 150,
-                width: 200,
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.all(20),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 1, color: Colors.red),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.red.withOpacity(0.3),
-                        offset: Offset(3, 3),
-                        blurRadius: 10,
-                        spreadRadius: 2,
+
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NewsCard(),
+              NewsCard(),
+              Container(
+                  height: 150,
+                  width: 200,
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 1, color: Colors.red),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.red.withOpacity(0.3),
+                          offset: Offset(3, 3),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                        ),
+                      ]
+                      // shape: BoxShape.circle,
                       ),
-                    ]
-                    // shape: BoxShape.circle,
-                    ),
-                child: Text("container")
-                // Container(color: Colors.green),
+                  child: Text("container")
+                  // Container(color: Colors.green),
+                  ),
+              Container(
+                height: 200,
+                width: 100,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.green],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter),
                 ),
-            Container(
-              height: 200,
-              width: 100,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.blue, Colors.green],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter),
               ),
-            ),
-            // Container(height: 50, width: 30, color: Colors.purple),
-            // Container(height: 50, width: 30, color: Colors.pink),
-            // Container(height: 50, width: 30, color: Colors.green),
-            // Container(height: 50, width: 30, color: Colors.yellow),
-          ],
+              Container(
+                height: 500,
+                color: Colors.brown,
+              )
+              // Container(height: 50, width: 30, color: Colors.purple),
+              // Container(height: 50, width: 30, color: Colors.pink),
+              // Container(height: 50, width: 30, color: Colors.green),
+              // Container(height: 50, width: 30, color: Colors.yellow),
+            ],
+          ),
         ),
       ),
     );
